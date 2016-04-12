7abeea307167ce3d3da5e9acc5d4e9d7c3f1bca1
Greg Magdsick <greg.magdsick@gmail.com>
2016-04-12 10:36:56 -0700

    writing to new file now

--- greg_magdsick/lib/tcp_server.js
+++ greg_magdsick/lib/tcp_server.js
@@ -1,21 +1,10 @@
 const net = require('net');
 const writeLog = require(__dirname + '/writeLog');
 
-const server = net.createServer((socket) => {
-  socket.writeStream
-  // socket.on('data', (pack) => {
-  //   var date = new Date();
-  //   var time = getTime(date);
-  //   pack.pipe(writeLog(time));
-  // });
-});
 
-var getTime = (dateObj) => {
-  var h = dateObj.getHours();
-  var m = dateObj.getMinutes();
-  var s = dateObj.getSeconds();
-  return h + '_' + m + '_' + s;
-}
+const server = net.createServer((socket) => {
+  socket.pipe(writeLog());
+});
 
 server.listen(7000, () => {
   process.stdout.write('server listening on 7000\n');
--- greg_magdsick/lib/writeLog.js
+++ greg_magdsick/lib/writeLog.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
-const writeLog = module.exports = exports = (time) => {
-  fs.createWriteStream(__dirname + '/../dat/output' + time + '.txt');
+
+const writeLog = module.exports = exports = () => {
+  const date = new Date();
+  var time = date.getTime();
+  return fs.createWriteStream(__dirname + '/../dat/output' + time + '.txt');
 }
